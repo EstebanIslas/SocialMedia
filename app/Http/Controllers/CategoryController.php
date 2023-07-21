70b63763c57242768e93afaa8265200a51205a96
EstebanIslas <1717110261@utectulancingo.edu.mx>
2023-07-21 11:12:01 -0600

    Metodos post y put en Categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,6 +8,12 @@ use App\Models\Category;
 
 class CategoryController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('api.auth', ['except' => ['index', 'show']]);
+    }
+
     public function index()
     {
         $categories = Category::all();
@@ -37,5 +43,87 @@ class CategoryController extends Controller
             );
         }
         return response()->json($data, $data['code']);
+    }
+
+    public function store(Request $request)
+    {
+        #Obtener datos por post
+        $json = $request->input('json', null);
+        $params_array = json_decode($json, true);
+
+        if (!empty($params_array)) {
+            #Validar Datos
+            $validate = \Validator::make($params_array, [
+                'name' => 'required'
+            ]);
+    
+            #Guardar en la bd
+            if ($validate->fails()) {
+                $data = [
+                    'code'       => 404,
+                    'status'     => 'error',
+                    'message'    => 'La categoria no se guardo correctamente'
+                ];
+            }else{
+                $category = new Category();
+                $category->name = $params_array['name'];
+                $category->save();
+    
+                $data = [
+                    'code'       => 200,
+                    'status'     => 'success',
+                    'categories' => $category
+                ];
+            }
+
+        } else {
+            $data = [
+                'code'       => 404,
+                'status'     => 'error',
+                'message'    => 'Los campos de categoria estan vacios'
+            ];
+        }
+        
+        #Retornar Resultado
+        return response()->json($data, $data['code']);
+    }
+
+    public function update($id, Request $request)
+    {
+        #Obtener Datos por post
+        $json = $request->input('json', null);
+        $params_array = json_decode($json, true);
+
+        if (!empty($params_array)) {
+            
+            #Validar Datos
+            $validate = \Validator::make($params_array,[
+                'name' => 'required'
+            ]);
+            
+            #Omitir lo que no se va a actualizar
+            unset($params_array['id']);
+            unset($params_array['created_at']);
+            
+            #Actualizar registro en la bd
+            $category = Category::where('id', $id)->update($params_array);
+
+            $data = [
+                'code'       => 200,
+                'status'     => 'success',
+                'category'   => $params_array
+            ];
+            
+        } else {
+            $data = [
+                'code'       => 404,
+                'status'     => 'error',
+                'message'    => 'Los campos de categoria estan vacios'
+            ];
+        }
+
+        #Retornar Resultado
+        return response()->json($data, $data['code']);
+        
     }
 }
