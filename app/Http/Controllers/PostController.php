5280f6aaf1bf1e175c346cee749304b77d313c55
EstebanIslas <1717110261@utectulancingo.edu.mx>
2023-07-21 11:56:02 -0600

    Metodos post y put en Posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Models\Post;
+use App\Helpers\JwtAuth;
 
 class PostController extends Controller
 {
@@ -55,4 +56,112 @@ class PostController extends Controller
 
     }
 
+    public function store(Request $request)
+    {
+        #Obtener datos por Post
+        $json = $request->input('json', null);
+        $params = json_decode($json);
+        $params_array = json_decode($json, true);
+
+        if (!empty($params_array)) {
+            
+            #Obtener usuario identificado por token
+            $jwtAuth = new JwtAuth();
+            $token = $request->header('Authorization', null);
+            $user = $jwtAuth->checkToken($token, true);
+
+            #Validar datos
+            $validate = \Validator::make($params_array, [
+                'title'         => 'required',
+                'content'   => 'required',
+                'category_id'   => 'required',
+                'image'   => 'required'
+            ]);
+
+            if ($validate->fails()) {
+                $data = [
+                    'code'      => 400,
+                    'status'    => 'error',
+                    'message'   => 'Error al guardar post, faltan datos'
+                ];
+            } else {
+                #Guardar datos en Bd
+                $post = new Post();
+                $post->user_id = $user->sub;
+                $post->category_id = $params->category_id;
+                $post->title = $params->title;
+                $post->content = $params->content;
+                $post->image = $params->image;
+
+                $post->save();
+
+                $data = array(
+                    'code'       => 200,
+                    'status'     => 'success',
+                    'posts'      => $post
+                );
+            }
+                
+        } else {
+            $data = [
+                'code'      => 400,
+                'status'    => 'error',
+                'message'   => 'Error al guardar post, faltan datos'
+            ];
+        }
+        
+        #Retornar Resultado
+        return response()->json($data, $data['code']);
+    }
+
+    public function update($id, Request $request)
+    {
+        #Obtener datos por post
+        $json = $request->input('json', null);
+        $params_array = json_decode($json, true);
+
+        if (!empty($params_array)) {
+            
+            #Validar los datos
+            $validate = \Validator::make($params_array, [
+                'title' => 'required',
+                'content' => 'required',
+                'category_id' => 'required'
+            ]);
+
+            if ($validate->fails()) {
+                $data = [
+                    'code'       => 404,
+                    'status'     => 'error',
+                    'message'    => 'El post no se actualizó faltan datos'
+                ];
+            } else {
+                
+                #Omitir campos para no actualizar
+                unset($params_array['id']);
+                unset($params_array['user_id']);
+                unset($params_array['created_at']);
+                unset($params_array['user']);
+        
+                #actualizar datos en la Bd
+                $post = Post::where('id', $id)->update($params_array);
+        
+                $data = [
+                    'code'      => 200,
+                    'status'    => 'success',
+                    'message'   => $params_array
+                ];
+            }
+            
+        } else {
+            $data = [
+                'code'      => 400,
+                'status'    => 'error',
+                'message'   => 'Error al actualizar post'
+            ];
+        }
+        
+        #Retornar Resultado
+        return response()->json($data, $data['code']);
+    }
 }
